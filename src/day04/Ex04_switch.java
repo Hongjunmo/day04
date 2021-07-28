5a9fbeaee95f9fb21f7d89f1c6af57a31ada7e67
HongJunMo <wnsah717@daum.net>
2021-07-28 14:46:55 +0900

    quiz 02 풀이

--- src/day04/Ex04_switch.java
+++ src/day04/Ex04_switch.java
@@ -1,5 +1,24 @@
 package day04;
 
-public class Ex04_switch {
+import java.util.Scanner;
 
+public class Ex04_switch {
+	public static void main(String[] args) {
+
+		Scanner scan= new Scanner(System.in);
+		System.out.println("수 입력: ");
+	int	select = scan.nextInt();
+		switch (select) {
+		case 1: System.out.println("1.입력값 : " + select);
+		break;
+		case 2: System.out.println("2.입력값 : " + select);
+		break;
+		case 3: System.out.println("3.입력값 : " + select);
+		break;
+		default:
+			System.out.println("4. 1,2,3을 제외한 다른 값 입력");
+		}
+	
+	
+	}
 }
